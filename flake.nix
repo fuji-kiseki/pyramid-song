43372f73e96de4b093b328ccced943750759d6da
マッテオ <fuji-kiseki@tuta.io>
2026-02-14 03:01:34 -0500

    refactor(build)!: replace Makefile with nix build

--- flake.nix
+++ flake.nix
@@ -23,6 +23,35 @@
             ];
         };
       });
+
+      packages = eachSystem (pkgs: {
+        default = pkgs.stdenv.mkDerivation {
+          name = "pyramid-song";
+          src = self;
+
+          nativeBuildInputs = with pkgs; [
+            elmPackages.elm
+            tailwindcss_4
+          ];
+
+          configurePhase = pkgs.elmPackages.fetchElmDeps {
+            elmPackages = import ./elm-srcs.nix;
+            elmVersion = "0.19.1";
+            registryDat = ./registry.dat;
+          };
+
+          buildPhase = ''
+            elm make src/Main.elm --output=dist/Main.js --optimize
+            tailwindcss -i ./src/styles.css -o ./dist/styles.css --minify
+          '';
+
+          installPhase = ''
+            mkdir -p $out
+            cp -r index.html dist $out/
+          '';
+        };
+      });
+
       formatter = eachSystem (pkgs: pkgs.nixfmt-tree);
     };
 }
